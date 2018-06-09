034da68d93b56b5bb95201f4b4dbbbe5ac67b6a9
Kennix Lazuardi <kennixlazuardi@gmail.com>
2018-06-09 14:41:21 +0700

    movement is clear but have not tested yet, add some control input in Inputmanager, remake Player prefab

--- Assets/_Scripts/Movement/PlayerMovement.cs
+++ Assets/_Scripts/Movement/PlayerMovement.cs
@@ -3,14 +3,16 @@
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 6f;
-    Vector3 movement;
-    Rigidbody playerRigidbody;
-    Quaternion playerQuaternion;
+    private Vector3 movement;
+    private Rigidbody playerRigidbody;
+    private GameObject playerHead;
+    private Quaternion playerQuaternion;
 
     // Use this for initialization
     private void Start()
     {
         playerRigidbody = GetComponent<Rigidbody>();
+        playerHead = gameObject.transform.GetChild(1).gameObject;
     }
 
     // Update is called once per frame
@@ -18,27 +20,44 @@ public class PlayerMovement : MonoBehaviour
     {
         var h = Input.GetAxisRaw("Horizontal");
         var v = Input.GetAxisRaw("Vertical");
+        var head = Input.GetAxisRaw("arrow");
 
         Move(v);
-        Turn(h);
-
+        Turn(v, h);
+        headTurn(head);
     }
 
-    void Move(float v)
+    private void Move(float v)
     {
-        Vector3 movement = transform.forward * v * (speed/2) * Time.deltaTime;
-        
+        Vector3 movement = transform.forward * v * (speed / 2) * Time.deltaTime;
+
         playerRigidbody.MovePosition(playerRigidbody.position + movement);
     }
 
-    void Turn(float h)
+    private void Turn(float v, float h)
     {
-        float turn = h * (speed*2) * Time.deltaTime;
-        
-        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
-        
-        playerRigidbody.MoveRotation(playerRigidbody.rotation * turnRotation);
+        float turn = h * (speed * 2) * Time.deltaTime;
 
+        Quaternion turnRotation;
+
+        if (v < 0)
+        {
+            turnRotation = Quaternion.Euler(0f, -turn, 0f);
+        }
+        else
+        {
+            turnRotation = Quaternion.Euler(0f, turn, 0f);
+        }
+
+        playerRigidbody.MoveRotation(playerRigidbody.rotation * turnRotation);
     }
 
+    private void headTurn(float head)
+    {
+        float turn = head * (speed * 2) * Time.deltaTime;
+
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        playerHead.transform.Rotate(0f, turn, 0f);
+    }
 }
