68debb68ed54d370e528cc714e987abe2fee2cf7
Kennix Lazuardi <kennixlazuardi@gmail.com>
2018-06-09 12:39:17 +0700

    change the way player facing, add movement control in player also with it's logic

--- Assets/_Scripts/Movement/PlayerMovement.cs
+++ Assets/_Scripts/Movement/PlayerMovement.cs
@@ -1,16 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+    Vector3 movement;
+    Rigidbody playerRigidbody;
+    Quaternion playerQuaternion;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        var h = Input.GetAxisRaw("Horizontal");
+        var v = Input.GetAxisRaw("Vertical");
+
+        Move(v);
+        Turn(h);
+
+    }
+
+    void Move(float v)
+    {
+        Vector3 movement = transform.forward * v * (speed/2) * Time.deltaTime;
+        
+        playerRigidbody.MovePosition(playerRigidbody.position + movement);
+    }
+
+    void Turn(float h)
+    {
+        float turn = h * (speed*2) * Time.deltaTime;
+        
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+        
+        playerRigidbody.MoveRotation(playerRigidbody.rotation * turnRotation);
+
+    }
+
 }
